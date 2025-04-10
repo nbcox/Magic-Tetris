6edc77907871993168261ec89af8516b7023151f
Nick Cox <nbcox8@outlook.com>
2025-04-09 23:22:32 -0500

    Updated to support Java 17+

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Use modules to work with Java 17+.
+ * 
+ * @author Nick Cox
+ *
+ */
+module magic_tetris {
+	requires java.desktop;
+	requires junit;
+	requires javafx.swing;
+	requires javafx.media;
+}
